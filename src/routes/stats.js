84c6bec03d629d0556c5e653919078f60ef35a7b
AhmedQudaih <ahmedku0123@gmail.com>
2024-09-06 17:39:35 +0300

    Update stats.js

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -5,11 +5,11 @@ export default async ({ apiServer, UserModel, AccountModel }) => {
   const secrets = process.env.SECRETS.split(' ')
   apiServer.get('/v1/statistics/accounts/', async req => {
     await allowAccessTo(req, secrets, [{ type: 'admin' }])
-    return list(AccountModel, null, {...req.query, select: { createdAt: 1, deleted: 1 }})
+    return list(AccountModel, null, { ...req.query, select: { createdAt: 1, deleted: 1 } })
   })
 
   apiServer.get('/v1/statistics/users/', async req => {
     await allowAccessTo(req, secrets, [{ type: 'admin' }])
-    return list(UserModel, null, {...req.query, select: { createdAt: 0, deleted: 1 }})
+    return list(UserModel, null, { ...req.query, select: { createdAt: 0, deleted: 1 } })
   })
 }
